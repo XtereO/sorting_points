0d66d4ef301ce2eda31867fe6b5cf1aec1976e5d
XtereO <kolahola149@gmail.com>
2026-06-12 21:16:34 +0300

    feat: covering base tests for sorting_parallelly

--- src/sorting_parallelly.py
+++ src/sorting_parallelly.py
@@ -2,10 +2,21 @@ from typing import List
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def sort_points_parallelly_x(points: List[List[float]], showing_result=False) -> List[List[int]]:
-    res = []
+def sort_points_parallelly_x(points: List[List[float]], max_delta=1, showing_result=False) -> List[List[int]]:
+    if (len(points) == 0):
+        return []
+    
     indexed_points = list(enumerate(points))
     sorted_points_y = sorted(indexed_points, key=lambda ip: (ip[1][1], ip[1][0], ip[1][2]))
+
+    res = [[]]
+    fp = sorted_points_y[0]
+    for p in sorted_points_y:
+        if p[1][1]-fp[1][1]<max_delta:
+            res[-1].append(p[0])
+        else:
+            fp = p
+            res.append([fp[0]])
 
     if (showing_result):
         x_groups = []
@@ -14,13 +25,13 @@ def sort_points_parallelly_x(points: List[List[float]], showing_result=False) ->
             x_ordered = list(map(lambda i: points[i][0], group))
             y_ordered = list(map(lambda i: points[i][1], group))
             x_groups.append(x_ordered)
-            y_groups.append(y_groups)
+            y_groups.append(y_ordered)
 
             sns.scatterplot(x=x_ordered, y=y_ordered)
         plt.show()
 
         for i, x_g in enumerate(x_groups):
-            plt.plot(x_g, y_groups[i], marker='o')
+            plt.plot(x_g, y_groups[i])
         plt.show()
     
     return res
--- tests/test_parallel_algorithm.py
+++ tests/test_parallel_algorithm.py
@@ -4,17 +4,19 @@ from sorting_parallelly import sort_points_parallelly_x
 class TestSortingParallellyXAlgorithm:
     @pytest.fixture
     def sorting_points_test_base_cases(self):
-        """Fixture providing test cases for calculate_discount."""
+        default_max_delta = 1
         return [
         # (input points, showing result, output groups of points ids)
-            ([], False, []),
-            ([[2, 2]], False, [[0]])
+            ([], default_max_delta, False, []),
+            ([[2, 2, 3]], default_max_delta, False, [[0]]),
+            ([[1,1,1], [2, 1, 1], [1,2,1], [2,2,1]], default_max_delta, False, [[0, 1], [2, 3]]),
+            ([[2,1,1], [2,2,1], [1,1,1], [1,2,1]], default_max_delta, False, [[2, 0], [3, 1]]),
+
         ]
 
     def test_base_cases(self, sorting_points_test_base_cases):
-        """Test calculate_discount with multiple cases."""
-        for points, showing_result, expected_groups_ids in sorting_points_test_base_cases:
-            groups_ids = sort_points_parallelly_x(points, showing_result)
+        for points, max_delta, showing_result, expected_groups_ids in sorting_points_test_base_cases:
+            groups_ids = sort_points_parallelly_x(points, max_delta, showing_result)
             assert groups_ids == expected_groups_ids
     
-    # tests for picking pivot point, immutability 
+    # tests for immutability 
